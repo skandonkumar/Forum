0bd54ad56ca6872be926ae5b51e51cf5a727e6f1
Rakhee Singh <rakhee@Rakhees-MBP.fios-router.home>
2018-12-15 00:23:32 -0500

    Layout Components for the project

---
+++ components/ContributeForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+import { Router } from '../routes';
+
+import Campaign from '../ethereum/question';
+import web3 from '../ethereum/web3';
+
+export default class ContributeForm extends Component {
+
+    state = {
+        value: '',
+        errorMessage: '',
+        loading: false
+    };
+
+    onSubmit = async (event) => {
+
+        event.preventDefault();
+
+        const campaign = Campaign(this.props.address);
+
+        this.setState({ loading: true, errorMessage: '' });
+        try {
+
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+        }
+        catch(error) {
+            this.setState({ errorMessage: error.message });
+        }
+
+        this.setState({ loading: false, value: '' });
+
+
+    };
+
+    render() {
+
+        return(
+
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+                <Form.Field>
+                    <label>Amount to Contribute</label>
+                    <Input
+                        label='ether'
+                        labelPosition='right'
+                        value={this.state.value}
+                        onChange={event => this.setState({ value: event.target.value }) }
+                    />
+                </Form.Field>
+                <Message
+                    error
+                    header='Oops!'
+                    content={this.state.errorMessage}
+                />
+                <Button loading={this.state.loading} primary>Contribute!</Button>
+            </Form>
+        );
+
+
+    }
+}
